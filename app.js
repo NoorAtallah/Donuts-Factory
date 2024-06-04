d8ee248a056d8c406d19b669bbba41b9d62148c4
NoorAtallah <nooratallah1234@gmail.com>
2024-06-05 00:37:00 +0300

    new edits were added

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
+user_input = []
+
 var name = prompt("Please enter your name");
+user_input.push(name)
 var gender = prompt("Please enter your gender");
-if(gender==="female"){
+while(gender!=="female" && gender!=="male"){
+    gender = prompt("Please enter your gender");
+}
+user_input.push(gender)
+
+if (gender=="male"){
+    alert("Welcome, Mr." + "  "+ name);
+}
+
+else  {
     alert("Welcome, Ms." +"  "+ name);
 }
-else if(gender==="male"){
-    alert("Welcome, Mr." + "  "+ name);
 
-}
-else{
-    alert("Welcome,"+"  " + name)
-}
 
 var order= prompt("Would you like to order: Donut,  Coffee, Ice cream or Bakery ")
 alert("Your Order is being preaped, Please wait!")
+user_input.push(typeof(order), order )
+
+
 
 console.log("Customer: " + name + ", Order: " + order)
+
+console.log("user's inputs", user_input)
+
+
+for (let i=0; user_input.length<i;i++ ){
+    console.log(user_input[i])
+}
